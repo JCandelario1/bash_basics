16275b4dce03fbac8fbbfa4a0489c69aab9415c0
jcandelario1@ggc.edu <jcandelario1@ggc.edu>
2026-08-30 23:17:50 -0400

    Add change message

---
+++ my_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello Jadalynn Candelario!"
+echo "How old are you?" 
+read age
+echo 'Thanks Jadalynn! I see you are ' $age ' years old'
+
+echo "What school do you currently attend?"
+read school
+echo 'Nice! ' $school ' sounds like a great place to learn.'
